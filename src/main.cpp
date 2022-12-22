113209ce472089042d4ad581485122d1cf2f6037
theholypumpkin <trash.pascal@theschieferstein.com>
2022-12-22 11:28:14 +0100

    Lazy final solution by removing interrups

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,22 @@
 #include <LiquidCrystal.h>
 /*================================================================================================*/
 #define CCS_811_INTERRUPT_PIN 2
-#define CCS_811_nWAKE 10
-#define DHTPIN 9
+#define CCS_811_nWAKE 13
+#define DHTPIN 17
 #define DHTTYPE DHT11
-#define LCD_RS_PIN 8
-#define LCD_EN_PIN 7
-#define LCD_D4_PIN 6
-#define LCD_D5_PIN 5
-#define LCD_D6_PIN 4
-#define LCD_D7_PIN 3
+#define LCD_RS_PIN 16
+#define LCD_EN_PIN 4
+#define LCD_D0_PIN 5
+#define LCD_D1_PIN 6
+#define LCD_D2_PIN 7
+#define LCD_D3_PIN 8
+#define LCD_D4_PIN 9
+#define LCD_D5_PIN 10
+#define LCD_D6_PIN 15
+#define LCD_D7_PIN 12
+#define LCD_BACKLIGHT_PWM 11
 #define RED_LED_PIN 14
-#define YELLOW_LED_PIN 15
+#define YELLOW_LED_PIN 3
 /*================================================================================================*/
 enum statemachine_t
 {
@@ -36,39 +41,36 @@ volatile statemachine_t e_state = IDLE;
 volatile bool b_isrFlag = false; //a flag which is flipped inside an isr
 uint8_t 
 szLetter[8] = {0b01110, 0b10001, 0b10001, 0b10110, 0b10001, 0b10001, 0b10110, 0b10000},
-ueLetter[8] = {0b00000, 0b10001, 0b00000, 0b10001, 0b10001, 0b10001, 0b10011, 0b01101},
+ueLetter[8] = {0b10001, 0b00000, 0b10001, 0b10001, 0b10001, 0b10011, 0b01101, 0b00000},
 lowerTwo[8] = {0b00000, 0b00000, 0b00000, 0b01100, 0b10010, 0b00100, 0b01000, 0b11110},
 degreeSymbol[8] = {0b01100, 0b10010, 0b10010, 0b01100, 0b00000, 0b00000, 0b00000, 0b00000};
 /*________________________________________________________________________________________________*/
 DHT tempHmdSensor(DHTPIN, DHTTYPE); //Create the DHT object
 Adafruit_CCS811 co2Sensor;
-LiquidCrystal lcd(LCD_RS_PIN, LCD_EN_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
+LiquidCrystal lcd(LCD_RS_PIN, LCD_EN_PIN, 
+                  LCD_D0_PIN, LCD_D1_PIN, LCD_D2_PIN, LCD_D3_PIN, 
+                  LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 /*================================================================================================*/
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
     pinMode(CCS_811_nWAKE, OUTPUT);
     pinMode(YELLOW_LED_PIN, OUTPUT);
     pinMode(RED_LED_PIN, OUTPUT);
-    /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
-    * Attach hardware interrupt BEFORE Setting up the CCS 811 sensor, because of a race condition.
-    * It cloud happen, that the pin was already driven low by the sensor, before we attach the
-    * interrupt. When the pin is already low, we can no longer detect a falling edge, hence the ISR
-    * would never trigger.
-    */
-    attachInterrupt(digitalPinToInterrupt(CCS_811_INTERRUPT_PIN), setReadFlagISRCallback, FALLING);
+    digitalWrite(YELLOW_LED_PIN, HIGH);
+    digitalWrite(RED_LED_PIN, HIGH);
+    /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
     digitalWrite(CCS_811_nWAKE, LOW); //Enable logic engine of CCS811
     delayMicroseconds(55); // Time until active after nWAKE asserted = 50 us
     /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
     tempHmdSensor.begin();
     if(!co2Sensor.begin()){
         while(1){
-            bool state = digitalRead(LED_BUILTIN);
-            digitalWrite(LED_BUILTIN, !state);
+            bool state = digitalRead(YELLOW_LED_PIN);
+            digitalWrite(YELLOW_LED_PIN, !state);
+            digitalWrite(RED_LED_PIN, state);
             delay(500); //When failure blink LED rapidly
         }
     }
     co2Sensor.setDriveMode(CCS811_DRIVE_MODE_60SEC);
-    co2Sensor.enableInterrupt();
     /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
     delayMicroseconds(25); //Logic engine should run at least 20 us
     digitalWrite(CCS_811_nWAKE, HIGH); //Disable logic engine
@@ -80,6 +82,10 @@ void setup() {
     lcd.createChar(1, ueLetter);
     lcd.createChar(2, lowerTwo);
     lcd.createChar(3, degreeSymbol);
+    /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+    LCD Brigthness PWM*/
+    analogWrite(LCD_BACKLIGHT_PWM, 63);
+
 }
 /*________________________________________________________________________________________________*/
 /**
@@ -94,17 +100,18 @@ void loop() {
     static float temperatureValue, heatIndexValue, humidityValue;
     static bool b_ENVDataCorrection;
     /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
-    if(b_isrFlag){
+    digitalWrite(CCS_811_nWAKE, LOW); //Enable logic engine of CCS811
+    delayMicroseconds(55); // Time until active after nWAKE asserted = 50 us
+    if(co2Sensor.available()){
         e_state = READ_DHT_SENSOR; //This is safer than setting the value inside the ISR itself
-        b_isrFlag = false;
     }
+    delayMicroseconds(25); // Time until active after nWAKE asserted = 50 us
+    digitalWrite(CCS_811_nWAKE, HIGH); //Enable logic engine of CCS811
     /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
     switch (e_state)
     {
     case READ_DHT_SENSOR:
         b_ENVDataCorrection = readDHTSensor(temperatureValue, humidityValue, heatIndexValue);
-        e_state = READ_CCS_SENSOR;
-        break;
     /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
     case READ_CCS_SENSOR:
         if(b_ENVDataCorrection){
@@ -118,58 +125,48 @@ void loop() {
     case LCD_DISPLAY_CCS:
         if(b_ENVDataCorrection){
             lcd.clear();
-            lcd.print("CO");
-            lcd.write(uint8_t(2));
-            lcd.print(": ");
-            lcd.print(eco2Value);
+            lcd.print("CO"); lcd.write(uint8_t(2)); lcd.print(": ");
+            lcd.print(eco2Value); lcd.print(" ppm");
             lcd.setCursor(0,1);
             lcd.print("TVOC: ");
-            lcd.print(tvocValue);
+            lcd.print(tvocValue); lcd.print(" ppb");
             e_state = LCD_DISPLAY_TEMPERATURE;
-            delay(10000);
         }else{
             e_state = IDLE;
         }
-        if(tvocValue > 100){
-            digitalWrite(YELLOW_LED_PIN, LOW);
-            digitalWrite(RED_LED_PIN, HIGH);
-        }
-        else if (tvocValue > 25){
-            digitalWrite(YELLOW_LED_PIN, HIGH);
-            digitalWrite(RED_LED_PIN, HIGH);
-        }
-        else if (tvocValue > 10){
+        if(tvocValue > 250){
             digitalWrite(YELLOW_LED_PIN, HIGH);
             digitalWrite(RED_LED_PIN, LOW);
+        }
+        else if (tvocValue > 100){
+            digitalWrite(YELLOW_LED_PIN, LOW);
+            digitalWrite(RED_LED_PIN, LOW);
+        }
+        else if (tvocValue > 50){
+            digitalWrite(YELLOW_LED_PIN, LOW);
+            digitalWrite(RED_LED_PIN, HIGH);
         }
         else{
-            digitalWrite(RED_LED_PIN, LOW);
-            digitalWrite(YELLOW_LED_PIN, LOW);
+            digitalWrite(RED_LED_PIN, HIGH);
+            digitalWrite(YELLOW_LED_PIN, HIGH);
         }
+        delay(10000);
         break;
     /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
     case LCD_DISPLAY_TEMPERATURE:
         lcd.clear();
-        lcd.print("Geme");
-        lcd.write(uint8_t(0));
-        lcd.print("en: ");
-        lcd.print(temperatureValue);
-        lcd.write(uint8_t(3));
-        lcd.print("C");
+        lcd.print("Geme"); lcd.write(uint8_t(0)); lcd.print("en: ");
+        lcd.print(temperatureValue); lcd.write(uint8_t(3)); lcd.print("C");
         lcd.setCursor(0,1);
-        lcd.print("Gef");
-        lcd.write(uint8_t(1));
-        lcd.print("hlt: ");
-        lcd.print(heatIndexValue);
-        lcd.write(uint8_t(3));
-        lcd.print("C");
+        lcd.print("Gef"); lcd.write(uint8_t(1)); lcd.print("hlt: ");
+        lcd.print(heatIndexValue); lcd.write(uint8_t(3)); lcd.print("C");
         e_state = LCD_DISPLAY_HUMIDITY;
         delay(10000);
         break;
      /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
     case LCD_DISPLAY_HUMIDITY:
         lcd.clear();
-        lcd.print("Luftfeuchte");
+        lcd.print("Luftfeuchtigkeit");
         lcd.setCursor(0,1);
         lcd.print(humidityValue);
         lcd.print(" %");
@@ -179,17 +176,9 @@ void loop() {
     /*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   */
     case IDLE:
     default:
-        delay(10); //Slow down a little bit
+        delay(100); //Slow down a little bit
         break;
     }
-}
-/*________________________________________________________________________________________________*/
-/**
- * @brief Set the Read Flag ISR Callback
- * 
- */
-void setReadFlagISRCallback(){
-    b_isrFlag = true;
 }
 /*________________________________________________________________________________________________*/
 /**
@@ -245,7 +234,7 @@ bool readDHTSensor(float &temperatureValue, float &humidityValue, float &heatInd
     if (isnan(humidityValue) || isnan(temperatureValue)) {
         return false;
     }
-    tempHmdSensor.computeHeatIndex(false);
+    heatIndexValue = tempHmdSensor.computeHeatIndex(false);
     return true;
 } 
 /*________________________________________________________________________________________________*/
